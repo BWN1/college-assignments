6a35889ea4bbe93e65218cbf6d7de176dfb1e0f4
Brody Neumann <brodyneumann@gmail.com>
2021-09-19 21:53:51 -0400

    Added formatting to triangle and screenshotted results

--- week1/src/Week1.java
+++ week1/src/Week1.java
@@ -5,8 +5,14 @@ public class Week1 {
 		for (int i = 1; i <= MAX_ROWS; i++) {
 			int totalNumbers = (i * 2) - 1;
 			int midpoint = ((i * 2) / 2) - 1;
+			
+			// Print spaces to align triangle
+			for (int j = 0; j < MAX_ROWS - i; j++)
+				System.out.print("    ");
+			
+			// Print the numbers
 			for (int j = 1, numLoops = 0; numLoops < totalNumbers; ++numLoops) {
-				System.out.print(j + " ");
+				System.out.printf("%3d ", j);
 				j = (numLoops < midpoint) ? j *= 2 : j / 2;
 			}
 			System.out.println();
