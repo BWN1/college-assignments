e5d61e1258f63584121a35ec1a6723cb67e96d25
Brody Neumann <brodyneumann@gmail.com>
2021-10-09 02:19:28 -0400

    Updated routing for getAll functions

--- routes/api/bestSellers.js
+++ routes/api/bestSellers.js
@@ -8,7 +8,6 @@ const { handleValidResponse, handleErrorResponse } = require('./utils');
 
 router.get('/', async (req, res) => {
   const bestSellers = await getAllBestSellers();
-  console.log(bestSellers.length);
   if (bestSellers.length) handleValidResponse(res, bestSellers);
   else handleErrorResponse(req, res, 404, 'There are no best sellers');
 });
