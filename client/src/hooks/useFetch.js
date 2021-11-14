c9e11beb5e3f72ebda0509a93642877db99bb828
Brody Neumann <brodyneumann@gmail.com>
2021-11-13 21:49:16 -0500

    Update useFetch to return if the fetch is loading

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -3,18 +3,21 @@ import { API_URL } from '../staticConfig';
 
 export const useFetch = (apiRoute) => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const response = await fetch(`${API_URL}/api/${apiRoute}/`);
       if (response.ok) {
         const { data } = await response.json();
         setData(data);
+        setIsLoading(false);
       }
     };
 
     fetchData();
   }, [apiRoute]);
 
-  return data;
+  return { data, loading: isLoading };
 };
