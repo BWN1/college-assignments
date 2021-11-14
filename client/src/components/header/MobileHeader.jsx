2c441bb473ddfbce0cfbd8a1a73594fd06752a06
Brody Neumann <brodyneumann@gmail.com>
2021-11-14 01:50:07 -0500

    Show transition for opening and closing mobile meny

--- client/src/components/header/MobileHeader.jsx
+++ client/src/components/header/MobileHeader.jsx
@@ -1,10 +1,9 @@
 import { React, useState, useEffect } from 'react';
 import { ReactComponent as FullLogo } from '@images/logo-long.svg';
 import { ReactComponent as HamburgerMenuImg } from '@icons/menu.svg';
-import { ReactComponent as CloseMenuImg } from '@icons/cancel.svg';
 
 import { Button, Loading } from '@components';
-import { SearchBar } from './SearchBar';
+import { MobileMenu } from './MobileMenu';
 import { useCategories } from '@hooks';
 
 export const MobileHeader = () => {
@@ -27,30 +26,7 @@ export const MobileHeader = () => {
         <FullLogo className="header-logo" />
       </Button>
       {showMenu && (
-        <div className="fixed top-0 left-0 w-screen h-screen bg-gray-300 bg-opacity-50 z-10">
-          <div className="bg-white w-5/6 px-8 py-7 h-full">
-            <div className="grid grid-cols-mobile-menu items-center mb-4">
-              <CloseMenuImg
-                className="w-8"
-                onClick={() => setShowMenu(false)}
-              />
-              <Button link="/" className="justify-self-center">
-                <FullLogo />
-              </Button>
-            </div>
-            <div className="flex flex-col space-y-4 text-lg">
-              <SearchBar />
-              <Button link="/signup">Sign Up</Button>
-              <Button link="/products">Products</Button>
-              <div className="flex flex-col space-y-3 pt-2">
-                <h3 className="font-bold">Categories</h3>
-                {categories.map(({ display, link }) => (
-                  <Button link={link}>{display}</Button>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
+        <MobileMenu showMenu={setShowMenu} categories={categories} />
       )}
     </nav>
   );
---
+++ client/src/components/header/MobileMenu.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { ReactComponent as CloseMenuImg } from '@icons/cancel.svg';
+import { ReactComponent as FullLogo } from '@images/logo-long.svg';
+import { SearchBar } from './SearchBar';
+import { Button } from '@components';
+
+export const MobileMenu = ({ showMenu, categories }) => {
+  useEffect(() => {
+    document.getElementById('mobile-menu-shade').classList.remove('opacity-0');
+  });
+
+  const handleClick = () => {
+    const menu = document.getElementById('mobile-menu-shade');
+    menu.classList.add('opacity-0');
+    menu.addEventListener('transitionend', () => showMenu(false));
+    return menu.removeEventListener('transitionend', () => showMenu(false));
+  };
+
+  return (
+    <div
+      id="mobile-menu-shade"
+      className="fixed top-0 left-0 w-screen h-screen 
+            bg-gray-300 bg-opacity-50 z-10 opacity-0 transition-all duration-300"
+    >
+      <div id="mobile-menu" className="bg-white w-5/6 px-8 py-7 h-screen z-10">
+        <div className="grid grid-cols-mobile-menu items-center mb-4">
+          <CloseMenuImg className="w-8" onClick={handleClick} />
+          <Button link="/" className="justify-self-center">
+            <FullLogo />
+          </Button>
+        </div>
+        <div className="flex flex-col items-start space-y-4 text-lg">
+          <SearchBar />
+          <Button link="/signup">Sign Up</Button>
+          <Button link="/products">Products</Button>
+          <div className="flex flex-col items-start space-y-3 pt-2">
+            <h3 className="font-bold">Categories</h3>
+            {categories.map(({ display, link }) => (
+              <Button key={display} link={link}>
+                {display}
+              </Button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
