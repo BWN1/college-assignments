1f7004e4a802a664f32137ebbf946f319a7d649f
Brody Neumann <brodyneumann@gmail.com>
2021-11-13 23:13:42 -0500

    Updated color of loading icon

--- client/src/components/common/Loading.jsx
+++ client/src/components/common/Loading.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ReactComponent as LoadingAnimation } from '@icons/loading.svg';
 
-export const Loading = ({ width, height }) => {
-  return <LoadingAnimation width={width} height={height} />;
+export const Loading = ({ width, height, color }) => {
+  return <LoadingAnimation width={width} height={height} color={color} />;
 };
