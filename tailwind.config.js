052255f4e39236240acbe8b8fca1dc3ee46e6fb7
Brody Neumann <brodyneumann@gmail.com>
2021-12-10 10:50:58 -0500

    Migrate to Tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,6 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.{html,ts}'],
-  },
-  darkMode: false,
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
-    colors: {
-      black: colors.black,
-      gray: colors.gray,
-      green: colors.green,
-      blue: colors.blue,
-      red: colors.red,
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
